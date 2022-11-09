3d968e1e4229a6ad9c704a3b964e446f00c92d44
OceanShape <oceanshape700@gmail.com>
2022-11-10 01:33:14 +0900

    Add 2-7 default

---
+++ 2-8Transformations/shader/shader.fs
@@ -0,0 +1,13 @@
+#version 330 core
+
+in vec2 TexCoord;
+
+out vec4 FragColor;
+
+uniform sampler2D block;
+uniform sampler2D mario;
+
+void main()
+{
+   FragColor = mix(texture(block, TexCoord), texture(mario, TexCoord), 0.5);
+}
